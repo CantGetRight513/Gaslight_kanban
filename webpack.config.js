604698b0baba11521cdce35e382285c1c6d74d7d
Jordan Stroud <jordanstudiesmath@gmail.com>
2016-05-22 02:07:12 -0400

    Install and configure webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 /* Webpack configuration file */
 
 const path = require('path');
+const merge = require('webpack-merge'); // For splitting up Webpack Configuration
 
-const PATHS = {
+const TARGET = process.env.npm_lifecycle_event; // currently running process?
+
+const PATHS = { // File Paths
 	app: path.join(__dirname, 'app'),
 	build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
 	// Entry accepts a path or an object of entries.  We'll be using the
 	// latter form given it's convenient with more complex configurations.
 	entry: {
@@ -18,3 +21,15 @@ module.exports = {
 		filename: 'bundle.js'
 	}
 };
+
+// Default configuration. We will return this if
+// Webpack is called outside of npm.
+if(TARGET ==='start' || !TARGET) {
+	module.exports = merge(common, {});
+}  // If just starting dev server merge common with an empty object
+
+if(TARGET === 'build') {
+	module.exports = merge(common, {});
+}  // Same, but for if re building/packing (will change later?)
+
+// Merge Split points are for starting the dev server and for repacking/building(?)
