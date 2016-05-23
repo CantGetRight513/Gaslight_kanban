508af8a18fb2e48aecd02b252b844c3e1a96f2cc
Jordan Stroud <jordanstudiesmath@gmail.com>
2016-05-23 09:48:57 -0400

    Updated Index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,17 @@
-/* Index.js */
+/* Index.jsx */
+import "./main.css";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+/* Index.js (Old) 
 require('./main.css');
-var component = require('./component');
+var component = require('./component');  //OLD COMPONENT CAN BE DROPPED
 
 document.body.appendChild(component());
+*/
