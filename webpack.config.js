f46edca22c1be7869fec905960c74159afcc2939
Jordan Stroud <jordanstudiesmath@gmail.com>
2016-05-23 10:11:10 -0400

    Configure Babel HMR

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,8 @@ const PATHS = { // File Paths
 	build: path.join(__dirname, 'build')
 };
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
 	// Entry accepts a path or an object of entries.  We'll be using the
 	// latter form given it's convenient with more complex configurations.
