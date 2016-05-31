812f5afe69d6727c72d294147182f222b9bca242
Jordan Stroud <jordanstudiesmath@gmail.com>
2016-05-31 10:28:13 -0400

    work on Flux Architecture

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,34 +1,32 @@
-import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteAtore from '../stores/NoteStore';
 
 export default class App extends React.Component {
     
     constructor(props) {
         super(props);
         
-        this.state = {
-            notes: [
-                {
-                    id: uuid.v4(),
-                    task: 'Learn Webpack'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Learn React'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Do laundry'
-                }
-            ]
-        };
+        this.state = NoteStore.getState();
     }
+	
+	componentDidMount() {
+		NoteStore.listen(this.storeChanged);
+	}
+	
+	componentWillUnmount() {
+		NoteStore.unlisten(this.storeChanged);
+	}
+	
+	storeChanged = (state) => {
+		this.setState(state);
+	}
     render() {
         const notes = this.state.notes;
     return (
      <div>     
-       <button onClick={this.addNote}>+</button>
+       <button className="add-note" onClick={this.addNote}>+</button>
        <Notes notes={notes} 
 	     onEdit={this.editNote}
 		 onDelete={this.deleteNote} />
@@ -41,9 +39,7 @@ export default class App extends React.Component {
 	  // Avoid bubbling on edit(?)
 	  e.stopPropagation();
 	  
-	  this.setState({
-		  notes:this.state.notes.filter(note => note.id !== id)
-	  });
+	  NoteActions.delete(id);
   };
   
 	
@@ -53,39 +49,11 @@ export default class App extends React.Component {
       return;
     }
 
-    const notes = this.state.notes.map(note => {
-      if(note.id === id && task) {
-        note.task = task;
-      }
-
-      return note;
-    });
-
-    this.setState({notes});
+    NoteActions.update({id, task}); // ?
   };
 
-  // We are using an experimental feature known as property
-  // initializer here. It allows us to bind the method `this`
-  // to point at our *App* instance.
-  //
-  // Alternatively we could `bind` at `constructor` using
-  // a line, such as this.addNote = this.addNote.bind(this);
-  addNote = () => { //WORKS  - IGNORE VSCode ERROR
-    // It would be possible to write this in an imperative style.
-    // I.e., through `this.state.notes.push` and then
-    // `this.setState({notes: this.state.notes})` to commit.
-    //
-    // I tend to favor functional style whenever that makes sense.
-    // Even though it might take more code sometimes, I feel
-    // the benefits (easy to reason about, no side effects)
-    // more than make up for it.
-    //
-    // Libraries, such as Immutable.js, go a notch further.
-    this.setState({
-        notes: this.state.notes.concat([{
-            id: uuid.v4(),
-            task: 'New task'
-        }])
-    });
-  };    
+  addNote() {
+	  NoteActions.create({task: 'New tast'});
+  }
+      
  }
---
+++ app/libs/alt.js
@@ -0,0 +1,7 @@
+import Alt from 'alt'
+// import chromeDevug from 'alt-utils/lib/chromeDebug';
+
+const alt = new Alt();
+//chromeDebug(alt);
+
+export default alt;
